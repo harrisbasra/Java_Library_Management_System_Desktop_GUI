e74b588a98946fbdf351d6222d616a67ac695890
Harris Basra <harrisbasra@gmail.com>
2023-10-18 01:33:19 +0500

    Start Layout

--- src/LibraryGUI.java
+++ src/LibraryGUI.java
@@ -1,2 +1,59 @@
-package PACKAGE_NAME;public class LibraryGUI {
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LibraryGUI {
+    private JPanel rootPanel;
+
+    public LibraryGUI() {
+        String[] columnNames = {"Name", "Author", "Publication Year", "Read Item"};
+        DefaultTableModel tableModel = new DefaultTableModel(columnNames, 0);
+        JTable table = new JTable(tableModel);
+
+        JButton addItemButton = new JButton("Add Item");
+        JButton editItemButton = new JButton("Edit Item");
+        JButton deleteItemButton = new JButton("Delete Item");
+
+        addItemButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Add your code to add a new row to the table here
+            }
+        });
+
+        editItemButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Add your code to edit the selected item here
+            }
+        });
+
+        deleteItemButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // Add your code to delete the selected item here
+            }
+        });
+
+        rootPanel.setLayout(new BorderLayout());
+        rootPanel.add(new JScrollPane(table), BorderLayout.CENTER);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(addItemButton);
+        buttonPanel.add(editItemButton);
+        buttonPanel.add(deleteItemButton);
+
+        rootPanel.add(buttonPanel, BorderLayout.SOUTH);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("LibraryGUI");
+        frame.setContentPane(new LibraryGUI().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(600, 400); // Adjust the size as needed
+        frame.setVisible(true);
+    }
 }
